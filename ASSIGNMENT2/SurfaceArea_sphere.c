20ec5d7722179083d8d1c48e7d82e524aabab83a
Kipkoech Caleb Rono <kipkoechcaleb266@gmail.com>
2025-11-18 16:46:35 +0300

    Add files via upload

---
+++ ASSIGNMENT2/SurfaceArea_sphere.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double const PI = 3.142;
+    double R;
+
+    printf("Enter the radius of the sphere: ");
+    scanf("%lf", &R);
+
+    printf("The surface area is %.4lf", 4* PI*pow(R, 2));
+    return 0;
+}
